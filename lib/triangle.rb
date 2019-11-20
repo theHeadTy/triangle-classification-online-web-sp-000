2c063cdeebc951255d66ad7bde3b79102553889d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-20 06:20:47 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+class TriangleError < StandardError
+end
+
 class Triangle
-  # write code here
+  
+  TriangleError = Class.new(TriangleError)
+  
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(eq, is, sc)
+    @equilateral = eq
+    @isosceles = is
+    @scalene = sc
+  end
+
+  def kind
+    
+    if sides.any? { |side| side <= 0 } or sides[0] + sides[1] <= sides[2]
+      raise TriangleError, 'Invalid Triangle'
+    else
+      case sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else
+        :scalene
+      end
+    end
+    
+  end
+  
+  private
+  def sides
+    [equilateral, isosceles, scalene].sort
+  end
 end
